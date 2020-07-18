17e60fc720533ba34a079411f4556ae2f33d6c50
Alex Castillo <alex@castillo.io>
2020-07-18 15:28:57 -0600

    :rocket:

---
+++ README.md
@@ -0,0 +1,22 @@
+# brain-terminal
+
+Use the terminal to get inside the human brain.
+
+```sh
+$ npx brain-terminal
+```
+
+![brain-terminal](./img/brain-terminal.png)
+
+- 🧠 Visualizes real-time brainwaves
+- 🧘🏾 Quantify focus & calm scores
+- 📊 Plot frequency bands: Alpha, Beta, Delta, Theta & Gamma
+- ⚡ Device status, sleep mode, battery levels, and OS version
+- 👌 Exit terminal with your mind (trained command: `rightHandPinch`)
+- 🔑 Neurosity authentication & multiple device support
+
+## License
+
+Copyright (c) 2020 Alex Castillo & [Neurosity](https://neurosity.co).
+
+Released under [the MIT license](LICENSE).
---
+++ index.js
@@ -0,0 +1,79 @@
+const { Notion: Mind } = require("@neurosity/notion");
+const inquirer = require("inquirer");
+const { getLoginPrompts, getDevicesPrompt } = require("./lib/prompts");
+
+async function init() {
+  console.log("Login with your Neurosity account to access the brain");
+
+  const loginForm = await inquirer.prompt(getLoginPrompts());
+
+  const mind = new Mind({ autoSelectDevice: false });
+
+  await mind.login(loginForm);
+
+  const devices = await mind.getDevices();
+
+  const selectedDevice = await inquirer.prompt(
+    getDevicesPrompt(devices)
+  );
+
+  const info = await mind.selectDevice((devices) =>
+    devices.find(
+      (device) => device.deviceId === selectedDevice.deviceId
+    )
+  );
+
+  console.clear();
+
+  const { screen, charts } = require("./lib/ui");
+  const Brainwaves = require("./lib/brainwaves");
+  const Awareness = require("./lib/awareness");
+  const PowerByBand = require("./lib/powerByBand");
+  const { tween } = require("./lib/tween");
+  const { getStatusString } = require("./lib/status");
+
+  screen.render();
+
+  charts.deviceNickname.setContent(
+    `${info.deviceNickname} (OS v${info.osVersion})`
+  );
+
+  const brainwaves = new Brainwaves(info);
+  const awareness = new Awareness();
+  const powerByBand = new PowerByBand();
+
+  mind.brainwaves("raw").subscribe((epoch) => {
+    brainwaves.render(epoch);
+  });
+
+  mind.brainwaves("powerByBand").subscribe((bands) => {
+    powerByBand.render(bands.data);
+  });
+
+  mind
+    .focus()
+    .pipe(tween())
+    .subscribe((focus) => {
+      awareness.renderFocus(focus);
+    });
+
+  mind
+    .calm()
+    .pipe(tween())
+    .subscribe((calm) => {
+      awareness.renderCalm(calm);
+    });
+
+  mind.status().subscribe((status) => {
+    const statusString = getStatusString(status);
+    charts.status.setContent(statusString);
+  });
+
+  mind.kinesis("rightHandPinch").subscribe(() => {
+    process.exit();
+  });
+}
+
+module.exports = {
+  init
+};
